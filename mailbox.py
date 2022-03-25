37cba3c325899cf7416e9f21f9e47cb0b00710ce
Nathan Gearke <Nathan.Gearke@veteransunited.com>
2022-03-24 21:25:50 -0500

    Files ready

--- mailbox.py
+++
@@ -1,4 +0,0 @@
-def from_counter() -> None:
-    file_name = input("Enter a file name: ")
-
-from_counter()
---
+++ positive_elements.py
@@ -0,0 +1,5 @@
+def print_positive_tuple_elements() -> None:
+    # Expect and play with something like this: test_list = [(4, 5, 9), (-3, 2, 3), (-3, 5, 6), (4, -6)]
+    input_tuples = input("Enter a list of tuples ")
+
+print_positive_tuple_elements()
